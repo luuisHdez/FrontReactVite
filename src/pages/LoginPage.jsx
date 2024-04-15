4618729f7967fb2b6c791447a7cec62184d685f8
luuisHdez <93790893+luuisHdez@users.noreply.github.com>
2024-04-15 10:39:30 -0600

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -53,7 +53,7 @@ function LoginPage() {
                 {error && <p className="error bg-red-500 p-3 rounded-lg block w-full text-center">{error}</p>}
             </form>
             <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3"
-                    onClick={() => navigate("/register")}>Register</button>
+                   onClick={()=> navigate("/register")}>Register</button>
         </div>
     );
 }
